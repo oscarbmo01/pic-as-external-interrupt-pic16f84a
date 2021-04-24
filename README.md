f83c24609ba2c7f5052fab46fcd7380dbde4108c
Oscar BMO <oscar01bmo@gmail.com>
2021-04-24 06:23:33 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Hello World, PIC16F84A, pic-as compiler
+
+<img src="img/anime.png" width="20%" title="">
+
+## About The Project
+
+External interrupt RB0/INT for PIC16F84A, compiler pic-as.
+
+### Built with
+
+* MPLAB
+* PROTEUS
+
+## Getting Started
+
+### Prerequisites
+
+- Install electrical design software that supports PIC16f84A microcontroller simulation, for example Proteus Design Suite.  
+https://www.microchip.com/en-us/development-tools-tools-and-software/mplab-x-ide
+
+- Install IDE MPLAB XC8 PIC AND USE PIC-AS AS COMPILER.  
+https://www.labcenter.com/
+
+
+### Installing
+
+1. Clone the repo
+    ```sh
+    git clone https://github.com/oscarbmo01/pic-as-hello-world.git
+
+2. Use the main.s file to record the microcontroller PIC16F84A
+
+## Usage
+
+![Electric Scheme](/img/demo.svg "Electric Scheme")
+
+## Licence
+
+Distributed under the MIT License. See LICENSE for more information.
---
+++ main.s
@@ -0,0 +1,58 @@
+;-------------------------------------------------------------------------------
+; File: main.s
+; Author: Oscar Lira Gonzalez
+; Device: pic16f84a
+; Description: on led
+; Compilator: pic-as
+;-------------------------------------------------------------------------------
+    
+;-------------------------------------------------------------------------------
+; Libraries included	    
+;-------------------------------------------------------------------------------
+    
+PROCESSOR 16F84A
+#include <xc.inc>
+
+;-------------------------------------------------------------------------------
+; PIC16F84A Configuration Bit Settings
+;-------------------------------------------------------------------------------
+    
+; CONFIG
+  CONFIG FOSC = XT              ; Oscillator Selection bits (XT oscillator)
+  CONFIG WDTE = OFF		; Watchdog Timer (WDT enabled)
+  CONFIG PWRTE = OFF		; Power-up Timer Enable bit (Power-up Timer is disabled)
+  CONFIG CP = OFF		; Code Protection bit (Code protection disabled)
+  
+PSECT resetVec, class=CODE, delta=2, abs
+resetVec:
+    goto main
+
+PSECT extIntRb0, class=CODE, delta=2
+extIntRb0:
+    btfss INTF			; external interrupt flag bit test, skip if set		
+    goto exitInt
+    bsf PORTB, 3		; pin in high mode, on led
+    exitInt:
+	bcf INTCON, 4		; clear external interrupt flag bit
+	retfie			; return from interrupt
+	
+PSECT code
+main:
+    bsf	  STATUS, 5		; select bank 1
+    
+    bsf	  INTCON, 7		; global interrupt enable
+    bsf	  INTCON, 4		; external interrupt enable
+
+    bcf	  TRISB, 1		; config pin in output mode
+    bcf	  TRISB, 2		
+    bcf	  TRISB, 3		
+    
+    bcf   STATUS, 5		; select bank 0
+    
+    bcf	  PORTB, 3		; pin in low mode, off led
+loop:
+    bsf	  PORTB, 1		; pin in high mode, on led
+    bsf	  PORTB, 2
+    goto  loop			
+    
+    END resetVec    
